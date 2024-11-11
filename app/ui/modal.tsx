b496023774a41b380d3c2a602abd67445b58674f
Lorenzo Montero <lorenzomontero@Lorenzos-Air-2.attlocal.net>
2024-11-10 20:17:41 -0500

    Just making changes to the modal file

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -1,3 +1,12 @@
-const Modal = () => {};
+import { useState } from "react";
+
+interface ModalProps {
+  options: string[];
+  onSelect: (option: string) => void;
+}
+
+const Modal = ({ options, onSelect }: ModalProps) => {
+  return <dialog className="bg-background">{}</dialog>;
+};
 
 export default Modal;
