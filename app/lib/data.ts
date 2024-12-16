51b39c5d4e3eca066480575ccf920568239d8175
Lorenzo Montero <lorenzomontero@Lorenzos-Air-2.attlocal.net>
2024-12-16 16:31:26 -0500

    Created Data and completed the interface for the main lifts and variations

---
+++ app/lib/data.ts
@@ -0,0 +1,67 @@
+import { Lift } from "./interfaces";
+
+export const mainLifts: Lift[] = [
+  {
+    name: "High BarSquat",
+    converstion: 1,
+    variation: "squat",
+  },
+  {
+    name: "Low Bar Squat",
+    converstion: 1,
+    variation: "squat",
+  },
+  {
+    name: "Bench Press",
+    converstion: 2,
+    variation: "bench",
+  },
+  {
+    name: "Deadlift",
+    converstion: 3,
+    variation: "deadlift",
+  },
+];
+
+export const Variations: Lift[] = [
+  {
+    name: "Pause Squat",
+    converstion: 0.9, //This Might be wrong change later
+    variation: "squat",
+  },
+  {
+    name: "Pause Bench",
+    converstion: 0.9, //This Might be wrong change later
+    variation: "bench",
+  },
+  {
+    name: "Pause Deadlift",
+    converstion: 0.9, //This Might be wrong change later
+    variation: "deadlift",
+  },
+  {
+    name: "Romanian Deadlift",
+    converstion: 0.8, //This Might be wrong change later
+    variation: "deadlift",
+  },
+  {
+    name: "Safety Squat Bar",
+    converstion: 0.7, //This Might be wrong change later
+    variation: "squat",
+  },
+  {
+    name: "Close Grip Bench",
+    converstion: 0.85, //This Might be wrong change later
+    variation: "bench",
+  },
+  {
+    name: "Larson Press",
+    converstion: 0.85, //This Might be wrong change later
+    variation: "bench",
+  },
+  {
+    name: "Board Press",
+    converstion: 0.85, //This Might be wrong change later
+    variation: "bench",
+  },
+];
--- app/lib/interfaces.ts
+++ app/lib/interfaces.ts
@@ -1,6 +1,5 @@
 export interface Lift {
   name: string;
   converstion: number;
-  category: "squat" | "bench" | "deadlift";
-  isVariation: boolean;
+  variation: "squat" | "bench" | "deadlift";
 }
