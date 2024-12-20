22cf19856cdd84a794a255f34d62ce30e3a3a879
Lorenzo Montero <lorenzomontero@Lorenzos-Air-2.attlocal.net>
2024-12-20 09:45:17 -0500

    Successfully added logo to the url bar using nextjs file conventions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,12 @@ import "./globals.css";
 import Navbar from "./components/navbar";
 import { Inter } from "next/font/google";
 import Footer from "./components/footer";
-import yuBarbellLogo from "./public/yuBarbellLogo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Yu Barbell",
   description: "Convert Your Lifts",
-  icons: {
-    icon: "./public/yuBarbellLogo.png",
-  },
 };
 
 export default function RootLayout({
@@ -22,14 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          href={`${yuBarbellLogo}`}
-          type="image/png"
-          sizes="32x32"
-        />
-      </head>
       <body className={`${inter.className} bg-background`}>
         <Navbar />
         <main className="flex flex-col justify-center text-text">
